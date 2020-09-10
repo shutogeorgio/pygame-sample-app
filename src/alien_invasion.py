36522e89627a66e3bab795a84fa8985cd1181663
shuGeorgio <dev.shudiscrete@gmail.com>
2020-09-10 20:24:12 +0900

    add:quick

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -30,11 +30,10 @@ class AlienInvasion:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         sys.exit()
-
       elif event.type == pygame.KEYDOWN: 
         self._check_keydown_events(event)
       elif event.type == pygame.KEYUP:    
-        self._check_keydown_events(event)
+        self._check_keyup_events(event)
           
     # Redraw the screen during each pass through the loop. 
     self.screen.fill(self.settings.bg_color)
@@ -51,12 +50,14 @@ class AlienInvasion:
       self.ship.moving_right = True 
     elif event.key == pygame.K_LEFT: 
       self.ship.moving_left = True
+    elif event.key == pygame.K_q: sys.exit()
 
   def _check_keyup_events(self, event):
     if event.key == pygame.K_RIGHT: 
       self.ship.moving_right = False
     elif event.key == pygame.K_LEFT: 
       self.ship.moving_left = False
+    elif event.key == pygame.K_q: sys.exit()
 
 if __name__ == '__main__':
   ai = AlienInvasion()
