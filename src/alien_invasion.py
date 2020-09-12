0abe966ec141abc1397b3eefad88d8402080754d
shuGeorgio <dev.shudiscrete@gmail.com>
2020-09-12 12:46:29 +0900

    add:something

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
 
@@ -26,6 +27,7 @@ class AlienInvasion:
     while True:
       self._check_events()
       self.ship.update()
+      self.bullets.update()
       self._update_screen()
   
   # Define Check Event Feature
@@ -42,25 +44,35 @@ class AlienInvasion:
     self.screen.fill(self.settings.bg_color)
     self.ship.blitme()
     pygame.display.flip()
-
-  # Define Update Event Feature
-  def _update_screen(self):
-    self.screen.fill(self.settings.bg_color)
-    self.ship.blitme()
   
   def _check_keydown_events(self, event):
     if event.key == pygame.K_RIGHT:
       self.ship.moving_right = True 
     elif event.key == pygame.K_LEFT: 
       self.ship.moving_left = True
-    elif event.key == pygame.K_q: sys.exit()
+    elif event.key == pygame.K_q: 
+      sys.exit()
+    elif event.key == pygame.K_SPACE:
+      self._fire_bullet()
 
   def _check_keyup_events(self, event):
     if event.key == pygame.K_RIGHT: 
       self.ship.moving_right = False
     elif event.key == pygame.K_LEFT: 
       self.ship.moving_left = False
-    elif event.key == pygame.K_q: sys.exit()
+    elif event.key == pygame.K_q: 
+      sys.exit()
+
+  def _fire_bullet(self):
+    new_bullet = Bullet(self)
+    self.bullets.add(new_bullet)
+  
+  # Define Update Event Feature
+  def _update_screen(self):
+    self.screen.fill(self.settings.bg_color)
+    self.ship.blitme()
+    for bullet in self.bullets.sprites(): 
+      bullet.draw_bullet()
 
 if __name__ == '__main__':
   ai = AlienInvasion()
--- src/bullet.py
+++ src/bullet.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame.sprites import Sprite
+from pygame.sprite import Sprite
 
 class Bullet(Sprite):
 
@@ -11,7 +11,7 @@ class Bullet(Sprite):
 
     # Create a bullet rect at (0, 0) and then set correct position.
     self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
-    self.rect.midtop = ai_game.shjip.rect.midtop
+    self.rect.midtop = ai_game.ship.rect.midtop
 
     # Store the bullet's position as a decimal value
     self.y = float(self.rect.y)
