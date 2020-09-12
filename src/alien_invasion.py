20bf57edf044251c34cd70488aab5a4ba53cff32
shuGeorgio <dev.shudiscrete@gmail.com>
2020-09-12 13:50:58 +0900

    add:fullRowOfAliens

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -91,17 +91,29 @@ class AlienInvasion:
     # make an alien
     # Align raw of aliens
     alien = Alien(self)
-    alien_width = alien.rect.width
+    alien_width, alien_height = alien.rect.size
     self.aliens.add(alien)
     available_space_x = self.settings.screen_width - (2 * alien_width)
     number_aliens_x = available_space_x // (2 * alien_width)
 
-    # Create the first row of aliens
-    for alien_number in range(number_aliens_x):
-      alien = Alien(self)
-      alien.x = alien_width + 2 * alien_width * alien_number
-      alien.rect.x = alien.x
-      self.aliens.add(alien)
+    # Determine the number of rows of aliens that fit on the screen
+    ship_height  = self.ship.rect.height
+    available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+    number_rows = available_space_y // (2 * alien_height)
+
+    for row_number in range(number_rows):
+      for alien_number in range(number_aliens_x):
+        self._create_alien(alien_number, row_number)
+  
+  def _create_alien(self, alien_number, row_number):
+    # Create the first row of alien
+    alien = Alien(self)
+    alien_width, alien_height = alien.rect.size
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+    self.aliens.add(alien)
+
 
   # Define Update Event Feature
   def _update_screen(self):
