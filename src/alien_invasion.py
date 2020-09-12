09f8be4819541d6c980d96347673948a3dcf3c5b
shuGeorgio <dev.shudiscrete@gmail.com>
2020-09-12 15:16:40 +0900

    add:collisionsFunc

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -86,6 +86,16 @@ class AlienInvasion:
     for bullet in self.bullets.copy():
       if bullet.rect.bottom <= 0:
         self.bullets.remove(bullet)
+    
+    self._check_bullet_alien_collisions()
+    
+  def _check_bullet_alien_collisions(self):
+    collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+    if not self.aliens:
+      # Destroy existing bullets and create new fleet
+      self.bullets.empty()
+      self._create_fleet()
   
   # Create Alien
   def _create_fleet(self):
@@ -133,6 +143,9 @@ class AlienInvasion:
     """
     self._check_fleet_edges()
     self.aliens.update()
+
+    if pygame.sprite.spritecollideany(self.ship, self.aliens):
+      print("Ship hit!!!")
 
   # Define Update Event Feature
   def _update_screen(self):
